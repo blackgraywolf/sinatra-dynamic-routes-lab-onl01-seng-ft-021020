fc34b7e0c5d30ca7735c2be1678d9f62dd2913f2
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-25 22:53:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,24 +6,8 @@ class App < Sinatra::Base
     @reverse_name
   end
 
-# get '/square/:number' do 
-#   # @square = params[:number].to_i ** 2
-#   # @square.to_i
-#   @num = params[:number].to_i ** 2
-#   @num.to_s
-# end
-
-get '/square/:number' do
-    @num = params[:number].to_i ** 2
-    @num.to_s
-  end
-  
-    get '/say/:number/:phrase' do
-    str = ""
-    @num = params[:number].to_i
-    @phrase = params[:phrase]
-    @num.times { str += "#{@phrase}\n" }
-    str
-  end
-
+ get '/square/:number' do 
+   @square = params[:number].to_i ** 2
+   @square
+end
 end
