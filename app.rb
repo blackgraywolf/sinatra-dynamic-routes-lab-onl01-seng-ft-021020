7607844207711da574c6de6a1df20b310d9a64f8
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-03-25 22:52:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -6,11 +6,11 @@ class App < Sinatra::Base
     @reverse_name
   end
 
- get '/square/:number' do 
-  # @square = params[:number].to_i ** 2
-  # @square.to_i
-   @num = params[:number].to_i ** 2
-   @num.to_s
-end
+# get '/square/:number' do 
+#   # @square = params[:number].to_i ** 2
+#   # @square.to_i
+#   @num = params[:number].to_i ** 2
+#   @num.to_s
+# end
 
 end
